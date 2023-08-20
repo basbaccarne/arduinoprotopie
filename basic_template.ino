99b26191079d64bb409abb8409c1b32ae94ca8d5
Bas Baccarne <bastiaan.baccarne@ugent.be>
2023-08-20 14:31:05 +0200

    Create basic_template.ino

---
+++ basic_template.ino
@@ -0,0 +1,100 @@
+/* 
+this code is the basic Arduino code to send some data and receive some data over serial
+by Bas Baccarne
+
+input: rotary sensor - variable name "rotary" // values 0 to 100
+output: neopixel ledring - variable "ledring" // values 0 to 24 (the ledring has 24 leds)
+*/
+
+// libraries
+#include "Adafruit_NeoPixel.h" // to control the ledring
+#include "string.h" // to manage the incoming serial data (character)
+
+// variables for the rotary sensor (connected to A0)
+#define ROTARY_ANGLE_SENSOR A0
+#define ADC_REF 5
+#define GROVE_VCC 5
+#define FULL_ANGLE 300
+float old_angle;
+
+// variables for the neopixel ledring (connected to D2)
+#define PIXEL_PIN   2
+#define PIXEL_COUNT 24
+Adafruit_NeoPixel strip = Adafruit_NeoPixel(PIXEL_COUNT, PIXEL_PIN, NEO_GRB + NEO_KHZ800);
+uint32_t color = strip.Color(75, 250, 100); // change color here if you want
+
+// manage incoming protopie message 1: declare struct to capture incoming data
+struct MessageValue {
+  String message;
+  String value;
+};
+
+// manage incoming protopie message 2: declare function to parse incoming protopie data
+struct MessageValue getMessage(String inputtedStr) {
+  struct MessageValue result;
+  char charArr[50];
+  inputtedStr.toCharArray(charArr, 50);
+  char* ptr = strtok(charArr, "||");
+  result.message = String(ptr);
+  ptr = strtok(NULL, "||");
+  if (ptr == NULL) {
+    result.value = String("");
+    return result;
+  }
+  result.value = String(ptr);
+  return result;
+}
+
+// manage incoming protopie message 3: declare MessageValue struct's instance for incoming protopie data
+struct MessageValue receivedData;
+
+///////////////////////
+void setup()
+//////////////////////
+{
+    // start serial communication
+    Serial.begin(9600);
+    Serial.setTimeout(10); 
+
+    // initiate input: rotary sensor
+    pinMode(ROTARY_ANGLE_SENSOR, INPUT);
+
+    // initiate output: neopixel led ring
+    strip.setBrightness(155); // set brightness on a scale from 0 to255
+    strip.begin();
+    strip.show(); // initialize all pixels to 'off'
+}
+
+//////////////////////
+void loop()
+//////////////////////
+{   
+    // input: read value of the rotary angle sensor
+    float voltage;
+    int sensor_value = analogRead(ROTARY_ANGLE_SENSOR);
+    voltage = (float)sensor_value*ADC_REF/1023;
+    float degrees = (voltage*FULL_ANGLE)/GROVE_VCC;
+    float angle = ceil(degrees/ 215 * 100);
+
+    // input: send value of the sensor to protopie if value changes (format: variable_name||value)
+    if(old_angle != angle){
+      Serial.print("rotary||");
+      Serial.println(angle, 0);
+      old_angle = angle;
+      delay(10);
+    }
+
+    // output: read message from serial
+    while (Serial.available() > 0) {
+      String receivedString = Serial.readStringUntil('\0'); // read until Serial buffer is empty
+      receivedData = getMessage(receivedString);
+    }
+   
+    // output: check variable name and act when the right variable name is detected (in this case: "ledring" is sent by protopie)
+      if(receivedData.message.equals("ledring")){
+        // fill the ledring from led n° 0 to the value of the variable "ledring"
+        strip.clear();
+        strip.fill(color, 0, receivedData.value.toInt());
+        strip.show();  
+      }
+}
